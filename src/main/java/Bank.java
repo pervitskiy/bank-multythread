9b59b372efe36d531538513b5e0319a12c86ea6b
pervitsky <pervitsky@cs.vsu.ru>
2021-04-25 12:20:53 +0300

    fix

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
-    /
     private List<Teller> tellers = new ArrayList<>();
     private int countTeller;
     private Cashbox cashbox;
